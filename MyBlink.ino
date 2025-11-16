d7d0dcfd84a8c099f9e05086bbc829c5f6c869b2
Kate <embeddedkate@gmail.com>
2025-11-16 13:32:03 +0100

    Master (#1)
    * First commit
    
    * First commit

---
+++ MyBlink.ino
@@ -0,0 +1,16 @@
+#ifndef BLINK_FREQUENCY
+#define BLINK_FREQUENCY 1000
+#endif
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);      // turn the LED on (HIGH is the voltage level)
+  delay(BLINK_FREQUENCY);               // wait for a BLINK_FREQUENCY ms
+  digitalWrite(LED_BUILTIN, LOW);       // turn the LED off by making the voltage LOW
+  delay(BLINK_FREQUENCY);               // wait for a BLINK_FREQUENCY ms
+}
